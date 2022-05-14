f99fbb312b33e73ce8d72d13c2c2e5b232f360ad
ZaykoK <26259596+zaykoK@users.noreply.github.com>
2022-05-14 16:42:52 +0200

    file date element

--- src/FileMetaData.jsx
+++ src/FileMetaData.jsx
@@ -21,12 +21,27 @@ export function FileMetaData(props) {
     const inbox = props.metadata.is_inbox
     const size = props.metadata.size
     const hash = props.metadata.hash
+    const date = props.metadata.time_modified
+
+    //lifted from https://stackoverflow.com/questions/847185/convert-a-unix-timestamp-to-time-in-javascript
+    function timeConverter(UNIX_timestamp) {
+        var a = new Date(UNIX_timestamp * 1000);
+        var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+        var year = a.getFullYear();
+        var month = months[a.getMonth()];
+        var date = a.getDate();
+        var hour = a.getHours();
+        var min = a.getMinutes();
+        var sec = a.getSeconds();
+        var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec;
+        return time;
+    }
 
     let links = []
     for (let el in urls) {
         let s = urls[el].split('/')
         let ss = s[2]
-        links.push(<p key={hash+'link'+urls[el]} style={{ margin: '1px' }} ><a style={{ color: 'lightBlue', textDecoration: 'none' }} href={urls[el]} >{ss}</a></p>)
+        links.push(<p key={hash + 'link' + urls[el]} style={{ margin: '1px' }} ><a style={{ color: 'lightBlue', textDecoration: 'none' }} href={urls[el]} >{ss}</a></p>)
     }
 
     const metadataStyle = {
@@ -38,10 +53,11 @@ export function FileMetaData(props) {
     }
 
     return <div style={metadataStyle}>
-        <p key={hash+'res'} style={{ margin: '1px' }}>Resolution: {imageSize[0]}x{imageSize[1]}</p>
-        <p key={hash+'size'} style={{ margin: '1px' }}>Size: {formatBytes(size)}</p>
-        <p key={hash+'contenttype'} style={{ margin: '1px' }}>Content-type : {mime}</p>
-        <p key={hash+'inbox'} style={{ margin: '1px' }}>{(inbox) ? ('Is not archived') : ('Is archived')}</p>
-        <div key={hash+'links'} style={{ margin: '1px' }}>Links:{links}</div>
+        <p key={hash + 'date'} style={{ margin: '1px' }}>Date: {timeConverter(date)}</p>
+        <p key={hash + 'res'} style={{ margin: '1px' }}>Resolution: {imageSize[0]}x{imageSize[1]}</p>
+        <p key={hash + 'size'} style={{ margin: '1px' }}>Size: {formatBytes(size)}</p>
+        <p key={hash + 'contenttype'} style={{ margin: '1px' }}>Content-type : {mime}</p>
+        <p key={hash + 'inbox'} style={{ margin: '1px' }}>{(inbox) ? ('Is not archived') : ('Is archived')}</p>
+        <div key={hash + 'links'} style={{ margin: '1px' }}>Links:{links}</div>
     </div>
 }
