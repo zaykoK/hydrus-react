c32e37a99ebaece933477e2159b1a3b2a766b3c9
ZaykoK <26259596+zaykoK@users.noreply.github.com>
2022-08-26 20:48:09 +0200

    count widget now gets file count on its own

--- src/Thumbnail/WidgetCount.tsx
+++ src/Thumbnail/WidgetCount.tsx
@@ -1,15 +1,32 @@
+import { useEffect, useState } from 'react';
 import './WidgetCount.css'
+import * as API from '../hydrus-backend'
 
 interface WidgetCountProps {
     count: number | undefined;
+    tag?: string;
 }
 
 function WidgetCount(props: WidgetCountProps) {
-    if (props.count === undefined || props.count === 1) { return <></> }
+    const [tagCount,setTagCount] = useState<number>(0)
 
+    async function getFileCount(tag:string) {
+        console.log(tag)
+        let response = await API.api_get_files_search_files({tags:[[tag]],return_hashes:true})
+        console.log(response.data.hashes.length)
+        setTagCount(response.data.hashes.length)
+    }
+
+    useEffect(() => {
+        if (props.tag !== undefined){
+            getFileCount(props.tag)
+        }
+    })
+
+    if (props.count === undefined || props.count === 1) { return <></> }
     let count;
     if (props.count !== undefined) { count = props.count }
 
-    return <div className="widgetCount">{count}</div>
+    return <div className="widgetCount">{tagCount}</div>
 }
 export default WidgetCount
