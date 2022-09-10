2e219ac560c6b1d32dbeb37c9bc5f80f042393fa
ZaykoK <26259596+zaykoK@users.noreply.github.com>
2022-09-10 23:07:28 +0200

    fixed es-lint warnings

--- src/FilePage/RelatedFiles.tsx
+++ src/FilePage/RelatedFiles.tsx
@@ -19,58 +19,61 @@ export function RelatedFiles(props: RelatedFilesProps) {
     const [thumbs, setThumbs] = useState<Array<JSX.Element>>([])
     const [scrollOffset, setScrollOffset] = useState<number>(0);
 
-    async function Search() {
-        if (props.tags === undefined) { return }
-        let list = props.tags
-        if (list.length === 0 || list === undefined) {
-            setRelatedHashes(list)
-            return
-        }
-        //Get file hashes
-        let response = await API.api_get_files_search_files({ tags: tagArrayToNestedArray(list), return_hashes: true, return_file_ids: false })
-        //For visual purpose reverse the order
-        let reverseHashes = response.data.hashes.slice().reverse()
-        //Put hashes in session storage for other elements to use
-        sessionStorage.setItem('group-hashes', JSON.stringify(reverseHashes))
-        setRelatedHashes(reverseHashes)
-    }
+    
 
     useEffect(() => {
+        async function Search() {
+            if (props.tags === undefined) { return }
+            let list = props.tags
+            if (list.length === 0 || list === undefined) {
+                setRelatedHashes(list)
+                return
+            }
+            //Get file hashes
+            let response = await API.api_get_files_search_files({ tags: tagArrayToNestedArray(list), return_hashes: true, return_file_ids: false })
+            //For visual purpose reverse the order
+            let reverseHashes = response.data.hashes.slice().reverse()
+            //Put hashes in session storage for other elements to use
+            sessionStorage.setItem('group-hashes', JSON.stringify(reverseHashes))
+            setRelatedHashes(reverseHashes)
+        }
+
         Search()
-    }, [])
+    }, [props.tags])
 
-    function returnCurrentImage(hash: string) {
-        if (props.currentHash === hash) {
-            return false
-        } else {
-            return true
-        }
-    }
+    
 
     useEffect(() => {
+        function isCurrentImage(hash: string) {
+            if (props.currentHash === hash) {
+                return true
+            }
+            return false
+        }
+
         let currentIndex = 0
         const windowSize = window.innerWidth
         // 27vw m
         // 7.5vw d
         let thumbHeight = 0
         if (isMobile()) {
-            thumbHeight = Math.min(130,((windowSize * (27/100)))) + 10
+            thumbHeight = Math.min(130, ((windowSize * (27 / 100)))) + 10
         }
         else {
-            thumbHeight = windowSize * (7.5/100) + 10
+            thumbHeight = windowSize * (7.5 / 100) + 10
         }
-        console.log(thumbHeight)
+        //console.log(thumbHeight)
 
         const SIZEOFLABEL = 18
 
-        const OFFSET=100
+        //const OFFSET = 100
 
 
         if (relatedHashes.length > 1) {
             let temp = []
             for (let hash in relatedHashes) {
-                let current = returnCurrentImage(relatedHashes[hash])
-                if (current === false) { currentIndex = parseInt(hash) }
+                let current = isCurrentImage(relatedHashes[hash])
+                if (current === true) { currentIndex = parseInt(hash) }
                 temp.push(
                     <ImageThumbnail
                         currentImage={current}
@@ -91,21 +94,23 @@ export function RelatedFiles(props: RelatedFilesProps) {
         else {
             setScrollOffset(SIZEOFLABEL + (currentIndex * thumbHeight))
         }
-        
+
     }, [relatedHashes, props.currentHash])
 
     useEffect(() => {
         //This scrolls the div with every image change
         const el = document.querySelector('.relatedStyle')
+        //Scroll vertically
         if (isMobile() && !isLandscapeMode()) {
             el?.scrollTo({ left: scrollOffset, top: 0, behavior: 'smooth' })
         }
-        else{
+        //Scroll horizontally
+        else {
             el?.scrollTo({ left: 0, top: scrollOffset, behavior: 'smooth' })
         }
-        
 
-    }, [thumbs])
+
+    }, [thumbs,scrollOffset])
 
     function getRelatedThumbsStyle(mobile: boolean): string {
         if (mobile) {
